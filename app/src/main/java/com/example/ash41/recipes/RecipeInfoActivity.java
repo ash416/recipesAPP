b05ab3039bb2f2c48133186a5cf11e530fba9ef5
Anna <ash416@mail.ru>
2018-03-02 18:14:36 +0300

    delete dataTask for recipeInfo

--- app/src/main/java/com/example/ash41/recipes/RecipeInfoActivity.java
+++ app/src/main/java/com/example/ash41/recipes/RecipeInfoActivity.java
@@ -1,14 +1,9 @@
 package com.example.ash41.recipes;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -18,71 +13,52 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class RecipeInfoActivity extends AppCompatActivity {
+    String recipeName;
+    String image;
+    String description;
+    String[] ingredients;
     String[] haveIngredients;
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_info);
+        dataInitialization();
+        setContext();
+    }
+
+    private void dataInitialization(){
         Intent intent = getIntent();
-        String recipeName = intent.getStringExtra("name");
+        recipeName = intent.getStringExtra("name");
+        image = intent.getStringExtra("image");
+        description = intent.getStringExtra("description");
+        ingredients = intent.getStringArrayExtra("ingredients");
         haveIngredients = intent.getStringArrayExtra("have_ingredients");
-        DatabaseTask dataTask = new DatabaseTask();
-        dataTask.execute(recipeName);
     }
 
-    class DatabaseTask extends AsyncTask<String, Void, Recipe> {
-        @Override
-        protected Recipe doInBackground(String... recipeName) {
-            Recipe recipe = null;
-            DatabaseAdapter dbAdapter = new DatabaseAdapter();
-            try {
-                recipe = dbAdapter.getData(recipeName[0]);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return recipe;
+    private void setContext(){
+        ImageView imageView = findViewById(R.id.recipeImage);
+        TextView textView = findViewById(R.id.recipeName);
+        textView.setText(recipeName);
+        TextView textViewIngr = findViewById(R.id.textViewIngr);
+        for (String ingr : ingredients) {
+            textViewIngr.append(getText(ingr));
         }
+        Picasso.with(imageView.getContext()).load(image).into(imageView);
 
-        @SuppressLint("SetTextI18n")
-        @Override
-        protected void onPostExecute(Recipe result) {
-            ImageView imageView = findViewById(R.id.recipeImage);
-            TextView textView = findViewById(R.id.recipeName);
-            textView.setText(result.getName());
-            String[] ingredients = result.getIngredients().toArray(new String[0]);
-            TextView textViewIngr = findViewById(R.id.textViewIngr);
-            for (String ingr : ingredients) {
-                textViewIngr.append(getText(ingr));
-            }
-            Picasso.with(imageView.getContext()).load(result.getImage()).into(imageView);
-
-            String description = result.getDescription();
-            int position = description.indexOf("\'");
-            String finalDescription = "";
-            while(position >= 0){
-                int nextPosition = description.indexOf("\'", position + 1);
-                finalDescription += description.substring(position + 1, nextPosition) + " ";
-                position = description.indexOf("\'", nextPosition + 1);
-            }
-            TextView textViewDescr = findViewById(R.id.textViewDesc);
-            textViewDescr.setText(finalDescription);
+        int position = description.indexOf("\'");
+        String finalDescription = "";
+        while(position >= 0){
+            int nextPosition = description.indexOf("\'", position + 1);
+            finalDescription += description.substring(position + 1, nextPosition) + " ";
+            position = description.indexOf("\'", nextPosition + 1);
         }
+        TextView textViewDescr = findViewById(R.id.textViewDesc);
+        textViewDescr.setText(finalDescription);
     }
-    protected Spannable getText(String ingredient){
+
+    private Spannable getText(String ingredient){
         Spannable text = new SpannableString(ingredient + "\n");
         for(String haveIngr: haveIngredients) {
             if (ingredient.toLowerCase().contains(haveIngr.toLowerCase())) {
