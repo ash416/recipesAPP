5a4de343d39aca8000c5228cce33f691ed50f52a
Anna <ash416@mail.ru>
2018-03-02 11:29:53 +0300

    union all parts

--- app/src/main/java/com/example/ash41/recipes/MainActivity.java
+++ app/src/main/java/com/example/ash41/recipes/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ash41.recipes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button ingredientsRecipesButton = (Button)findViewById(R.id.find_recipes_by_ing);
+        ingredientsRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        IngredientActivity.class);
+                startActivity(intent);
+            }
+        });
+        /*final Button nameRecipesButton = (Button)findViewById(R.id.find_recipes_by_name);
+        nameRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecipesSearchActivity.class);
+                startActivity(intent);
+            }
+        });*/
     }
 }
